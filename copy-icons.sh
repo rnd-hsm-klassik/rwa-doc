96114048615daaab1cb6f508c892deec8d8163e0
Cedric Spindler <cedric.spindler@fhnw.ch>
2026-09-13 14:54:21 +0200

    Add recordLandmarkButton

--- copy-icons.sh
+++ copy-icons.sh
@@ -23,6 +23,7 @@ declare -a FILES=(
     "delete_forever_48dp_434343_FILL0_wght300_GRAD-25_opsz48.svg:donttrashassets.svg"
     "not_listed_location_48dp_434343_FILL0_wght300_GRAD-25_opsz48.svg:findlocation.svg"
     "flag_48dp_2854C5_FILL1_wght300_GRAD-25_opsz48.svg:flag.svg"
+    "flag_add_48dp_434343_FILL0_wght300_GRAD-25_opsz48-custom.svg:recordLandmarkButton.svg"
     "podiatry_48dp_434343_FILL1_wght300_GRAD-25_opsz48.svg:headtrackerStepButton.svg"
     "select_48dp_434343_FILL0_wght300_GRAD-25_opsz48.svg:pen.svg"
     "radio_button_unchecked_48dp_434343_FILL0_wght300_GRAD-25_opsz48.svg:radiiVisibleButton.svg"
